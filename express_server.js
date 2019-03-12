7942229650f3fe01e9c0fe54d2dc80699ddf779e
Bonita Choi <bonitac93@gmail.com>
2019-03-12 21:59:38 +0000

    able to display list of urls and link to shortening the url

--- express_server.js
+++ express_server.js
@@ -1,11 +1,17 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+app.set('view engine', 'ejs');
 
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+var urlDatabase = [
+  {short: "b2xVn2", long:"http://www.lighthouselabs.ca"},
+  {short: "9sm5xK", long: "http://www.google.com"}
+];
+
+// var urlDatabase = {
+//   "b2xVn2": "http://www.lighthouselabs.ca",
+//   "9sm5xK":"http://www.google.com"
+// };
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -15,10 +21,7 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
