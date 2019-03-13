f3b13ef82692826f3f3fa83ea529b6bed16bc589
Bonita Choi <bonitac93@gmail.com>
2019-03-13 01:00:26 +0000

    added post to form route and generate random alphanumeric string of length 6

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 app.set('view engine', 'ejs');
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 var urlDatabase2 = [
   {short: "b2xVn2", long:"http://www.lighthouselabs.ca"},
@@ -31,10 +34,20 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
-app.get("/urls/:shortURL", (req, res) => {
+app.post("/urls", (req, res) => {
+  // console.log(req.body);  // Log the POST request body to the console
+  //   res.send("Ok");         // Respond with 'Ok' (we will replace this)
   // console.log(req);
-  // console.log(urlDatabase[req.params.shortURL])
+  let newLong = req.body;
+  let templateVars = { longURL: newLong.longURL};
+  res.render("urls_new",templateVars)
+});
+
+app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
+function generateRandomString() {
+  return Math.floor((1 + Math.random()) * 0x100000).toString(16).substring(0); 
+}
