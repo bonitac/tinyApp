97208115aa5f6f71dd8f8c7086711cffd8f2dfce
Bonita Choi <bonitac93@gmail.com>
2019-03-16 03:56:18 +0000

    before trying out passing in user through templatevars

--- express_server.js
+++ express_server.js
@@ -1,11 +1,11 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
 app.set('view engine', 'ejs');
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-var cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session')
 
 app.use(cookieSession({
   name: 'session',
@@ -14,6 +14,7 @@ app.use(cookieSession({
 }))
 const bcrypt = require('bcrypt');
 
+//Global Variables:
 const urlDatabase = {
   // b6UTxQ: { longURL: "https://www.tsn.ca", userID: "userRandomID" },
   // i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
@@ -32,6 +33,7 @@ const users = {
   }
 }
 
+//Functions
 //Generate random string of length 6
 function generateRandomString() {
   return Math.floor((1 + Math.random()) * 0x100000).toString(16).substring(0); 
